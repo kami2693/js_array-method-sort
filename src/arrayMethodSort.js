4dbb93afc34c74d1a36cc10d2b8ae9c41e5ced3a
kami2693 <kamikaze2693@gmail.com>
2025-03-24 11:18:38 +0100

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,57 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    // Create a new variable for the comparison function
+    const compareFn =
+      typeof compareFunction === 'function'
+        ? compareFunction
+        : (a, b) => {
+          if (a === b) {
+            return 0;
+          }
+
+          if (a === undefined) {
+            return 1;
+          }
+
+          if (b === undefined) {
+            return -1;
+          }
+
+          if (a === null) {
+            return 1;
+          }
+
+          if (b === null) {
+            return -1;
+          }
+
+          const aString = String(a);
+          const bString = String(b);
+
+          if (aString < bString) {
+            return -1;
+          }
+
+          if (aString > bString) {
+            return 1;
+          }
+
+          return 0;
+        };
+
+    // Bubble sort implementation
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compareFn(this[j], this[j + 1]) > 0) {
+          // Swap elements
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
